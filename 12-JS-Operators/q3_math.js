5e1f5f349587c6e4ac81adf1f05623f9e66bda0b
suhani0110 <suhanitomar10@gmail.com>
2025-11-25 14:24:05 +0530

    Added q3_math.js

---
+++ 12-JS-Operators/q3_math.js
@@ -0,0 +1,13 @@
+let x = 16.75;
+
+let roundedValue = Math.round(x);
+console.log("Rounded: " + roundedValue);
+
+let sqrtValue = Math.sqrt(x);
+console.log("Square root: " + sqrtValue);
+
+let powerValue = Math.pow(x, 3);
+console.log("Power of 3: " + powerValue);
+
+let randomNum = Math.floor(Math.random() * 41) + 10;
+console.log("Random number (10-50): " + randomNum);
